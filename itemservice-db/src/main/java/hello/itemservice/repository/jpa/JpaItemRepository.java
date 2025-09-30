8687d3f57a466078a257236c00415a4fad65aef2
1soo <aaa4799@naver.com>
2025-09-30 18:06:55 +0900

    2025-09-30 JPA 기초 설정

---
+++ itemservice-db/src/main/java/hello/itemservice/repository/jpa/JpaItemRepository.java
@@ -0,0 +1,59 @@
+package hello.itemservice.repository.jpa;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Repository
+@Transactional
+@RequiredArgsConstructor
+public class JpaItemRepository implements ItemRepository {
+
+    private final EntityManager em;
+
+    @Override
+    public Item save(Item item) {
+        em.persist(item);
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        Item findItem = em.find(Item.class, itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        return Optional.ofNullable(em.find(Item.class, id));
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String jpql = "select i from Item i where 1 = 1";
+
+        if(StringUtils.hasText(cond.getItemName())) jpql += " and i.itemName like concat('%', :itemName, '%')";
+        if(cond.getMaxPrice() != null) jpql += " and i.price <= :maxPrice";
+
+        TypedQuery<Item> query = em.createQuery(jpql, Item.class);
+
+        if(StringUtils.hasText(cond.getItemName())) query.setParameter("itemName", cond.getItemName());
+        if(cond.getMaxPrice() != null) query.setParameter("maxPrice", cond.getMaxPrice());
+
+        return query.getResultList();
+    }
+}
